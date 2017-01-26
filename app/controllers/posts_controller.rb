45cca1889b5441934da3e79b805f8b8650a674b6
Andy Walters <andywalters42@gmail.com>
2017-01-26 00:31:04 -0800

    user-developments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,17 @@
 class PostsController < ApplicationController
-  before_action :find_post, only: [:show, :edit, :update, :destroy] 
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
   	@posts = Post.all.order("created_at DESC")
   end
 
   def new
-  	@post = Post.new
+  	@post = current_user.posts.build
   end
 
   def create
-  	@post = Post.new(post_params)
+  	@post = current_user.posts.build(post_params)
 
   	if @post.save
   		redirect_to @post
@@ -47,6 +48,10 @@ class PostsController < ApplicationController
 
   def post_params
   	params.require(:post).permit(:title, :body)
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    redirect_to root_path
   end
 end
 
