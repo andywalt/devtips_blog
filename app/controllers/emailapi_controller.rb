6cb7e92526ac417882a2255612905a53f45aebd3
Andy Walters <andywalters42@gmail.com>
2017-02-23 16:37:06 -0800

    adjusted for status 400 error mailchimp

--- app/controllers/emailapi_controller.rb
+++ app/controllers/emailapi_controller.rb
@@ -6,8 +6,9 @@ class EmailapiController < ApplicationController
 	def subscribe
 		gb = Gibbon::Request.new(api_key: "7fd5c6c2ae53985888bbfa8a2ab149f6-us14")
 
-		gb.lists("1367b2dcb8").create.members({
-			:email => {:email => params[:email][:address]}
+		gb.lists("1367b2dcb8").create.members(
+			body {
+			:email => {:email => "testemail@gmail.com"}
 			})
 		redirect_to root_path
 	end
